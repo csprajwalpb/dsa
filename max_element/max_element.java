031f2740dfce5e62e54c963d75d621e564702cb7
Prajwal P B <prajwalpb8055@gmail.com>
2025-12-11 08:48:42 +0530

    Initial Commit

---
+++ max_element/max_element.java
@@ -0,0 +1,12 @@
+public class max_element{
+    public static void main(String[] args) {
+        int [] arr = {3,5,7,8,65,9,80};
+        int max = arr[0];
+        for(int i=1; i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
