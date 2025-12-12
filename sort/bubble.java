20c40166dff66b50498eac3b4dbb10c662851f1f
Prajwal P B <prajwalpb8055@gmail.com>
2025-12-12 07:50:22 +0530

    4

---
+++ sort/bubble.java
@@ -0,0 +1,35 @@
+public class bubble {
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+
+        bubbleSort(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            boolean swapped = false; 
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+        }
+    }
+}
+//Time Complexity: O(n^2) in the worst and average cases, 
+//O(n) in the best case (when the array is already sorted).
