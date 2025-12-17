95f8e0443a4373494844aa1e7f6f3b397b597223
Prajwal P B <prajwalpb8055@gmail.com>
2025-12-17 07:24:00 +0530

    9

---
+++ missing/missing.java
@@ -0,0 +1,18 @@
+public class missing {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5}; 
+        int n = 5;
+
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : arr) {
+            actualSum += num;
+        }
+
+        int missing = expectedSum - actualSum;
+
+        System.out.println("Missing number: " + missing);
+    }
+}
+//Time Complexity: O(n)
