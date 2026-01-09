996cfb9c36d828cc6b25f95a7c691dc87ecb2344
Prajwal P B <prajwalpb8055@gmail.com>
2026-01-09 09:58:47 +0530

    11

---
+++ removeduplicates/duplicates.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class duplicates {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        Set<Integer> set = new LinkedHashSet<>(); 
+
+        for (int num : arr) {
+            set.add(num);
+        }
+
+        System.out.println(set);
+    }
+}
+//Time Complexity: O(n)
